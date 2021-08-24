e732da6e3d4fe8dc4ec383762410afb31ade960c
nxppru <nxppru@qq.com>
2021-08-24 11:32:51 +0800

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-echo -e "======================启动xdd========================\n"
+echo -e "======================更新xdd========================\n"
+git pull
+echo -e "xdd更新成功...\n"
+
+echo -e "========停止容器配置conf/config.yaml再启动xdd==========\n"
 ./xdd -d 2>/dev/null
 echo -e "xdd启动成功...\n"
 
